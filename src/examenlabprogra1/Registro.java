01199d34c9bee52596505cfb6aa58533e910a0cc
Diegu Montes <Diegu Montes>
2016-08-26 09:04:41 -0600

    finito

---
+++ src/examenlabprogra1/Registro.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examenlabprogra1;
+
+/**
+ *
+ * @author Diegu7
+ */
+public class Registro {
+    
+    String nombre, apellido, numero, correo;
+    
+    
+    public Registro(String fname, String lname, String num, String mail){
+        nombre = fname;
+        apellido = lname;
+        numero = num;
+        correo = mail;
+    }
+}
